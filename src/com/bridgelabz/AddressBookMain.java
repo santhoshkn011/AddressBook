6b825be3f30e58bb447d69f081eed694cf4e0ab5
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-23 19:54:33 +0530

    Added: Welcome Message.

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,13 @@
+/*
+Start with Displaying
+Welcome to Address Book
+Program in
+AddressBookMain class
+ */
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String args[]){
+        System.out.println("Welcome to Address Book Program!");
+    }
+}

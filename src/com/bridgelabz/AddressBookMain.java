cad049c0299a49158889297b0c9744607536e72a
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-24 22:28:05 +0530

    Addes: Contact Class Created.

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,13 +1,29 @@
 /*
-Start with Displaying
-Welcome to Address Book
-Program in
-AddressBookMain class
+Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email address.
  */
 package com.bridgelabz;
 
 public class AddressBookMain {
     public static void main(String args[]){
+        //Greetings
         System.out.println("Welcome to Address Book Program!");
+        //Creating object
+        Contacts contacts = new Contacts();
+        contacts.setFirstName("Santhosh");
+        System.out.println("First Name: "+contacts.getFirstName());
+        contacts.setLastName("Kumar");
+        System.out.println("Last Name: "+contacts.getLastName());
+        contacts.setAddress("Patel Nagar");
+        System.out.println("Address: "+contacts.getAddress());
+        contacts.setCity("Hyderabad");
+        System.out.println("City: "+contacts.getCity());
+        contacts.setState("Telangana");
+        System.out.println("State: "+contacts.getState());
+        contacts.setZipCode("500011");
+        System.out.println("Pin Code: "+contacts.getZipCode());
+        contacts.setContactNumber("1234567890");
+        System.out.println("Contact Number: "+contacts.getContactNumber());
+        contacts.setEmailAddress("santhosh@gmail.com");
+        System.out.println("Email Address: "+contacts.getEmailAddress());
     }
 }
---
+++ src/com/bridgelabz/Contacts.java
@@ -0,0 +1,65 @@
+package com.bridgelabz;
+
+public class Contacts {
+    private String firstName, lastName, address, city, state, zipCode, contactNumber, emailAddress;
+    public void Contacts() {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.contactNumber = contactNumber;
+        this.emailAddress = emailAddress;
+    }
+    public String getFirstName () {
+        return firstName;
+    }
+    public void setFirstName (String firstName){
+        this.firstName = firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public  void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getAddress () {
+        return address;
+    }
+    public void setAddress (String address){
+        this.address = address;
+    }
+    public String getCity () {
+        return city;
+    }
+    public void setCity (String city){
+        this.city = city;
+    }
+    public String getState () {
+        return state;
+    }
+    public void setState (String state){
+        this.state = state;
+    }
+    public String getZipCode () {
+        return zipCode;
+    }
+    public void setZipCode (String zipCode){
+        this.zipCode = zipCode;
+    }
+    public String getContactNumber () {
+        return contactNumber;
+    }
+    public void setContactNumber (String contactNumber){
+        this.contactNumber = contactNumber;
+    }
+    public String getEmailAddress () {
+        return emailAddress;
+    }
+    public void setEmailAddress (String emailAddress){
+        this.emailAddress = emailAddress;
+    }
+}
+
+
